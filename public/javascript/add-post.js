cd5941ad04c5b251345453e949c0b1b7a8b96c57
angiebunk1 <103800228+angiebunk1@users.noreply.github.com>
2022-08-12 09:23:14 -0500

    fixed bugs

--- public/javascript/add-post.js
+++ public/javascript/add-post.js
@@ -1,8 +1,8 @@
 async function newFormHandler(event) {
   event.preventDefault();
 
-  const post_title = document.querySelector('input[id="post_title"]').value;
-  const post_content = document.querySelector('textarea[id="post_content"]').value;
+  const post_title = document.querySelector('input[id="post-title"]').value;
+  const post_content = document.querySelector('input[id="post-content"]').value;
 
   const response = await fetch(`/api/posts`, {
     method: 'POST',
--- public/javascript/edit-post.js
+++ public/javascript/edit-post.js
@@ -1,8 +1,8 @@
 async function editFormHandler(event) {
   event.preventDefault();
 
-  const post_title = document.querySelector('input[name="post_title"]').value.trim();
-  const post_content = document.querySelector('input[name="post_content"]').value.trim();
+  const post_title = document.querySelector('input[name="post-title"]').value.trim();
+  const post_content = document.querySelector('input[name="post-content"]').value.trim();
   const id = window.location.toString().split('/')[
     window.location.toString().split('/').length - 1
   ];
